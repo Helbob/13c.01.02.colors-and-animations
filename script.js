af3d170a99210eb6af31804b893ddb8faa7a9860
Helbob <millehelbo@outlook.dk>
2022-10-08 19:12:12 +0200

    fade

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ document.querySelectorAll("h2").forEach((item) => {
 */
 inView("section", ({ target }) => {
   animate(
-    target.querySelectorAll("h2"),
+    target.querySelector(".fade"),
     { opacity: 1, x: [-70, -50, 0] },
     { delay: 0.2, duration: 0.9, easing: [0.17, 0.55, 0.55, 1] }
   );
